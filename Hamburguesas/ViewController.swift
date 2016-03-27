e72639e6795242cf52510240eb0aea1c99d176a2
Luis Alejandro Villa Vargas <VillaVargas@mbp-de-luis-2.lan>
2016-03-27 13:42:55 -0600

    Tarea 3 hamburguesa

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mensajePaises: UILabel!
+    
+    
+    @IBOutlet weak var mensajeHamburguesa: UILabel!
+    
+    let colores = Colores()
+    
+    let paises = ColeccionDePaises()
+    
+    let hamburguesas = ColeccionDeHamburguesa()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +34,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+  
+  
+    @IBAction func dameUnaHamburguesa() {
+      mensajeHamburguesa.text = paises.obtenPais()
+      mensajePaises.text = hamburguesas.obtenHamburguesa()
+      let colorAleatorio = colores.regresaColorAleatorio()
+      view.backgroundColor = colorAleatorio
+      view.tintColor = colorAleatorio
+    }
 }
 
