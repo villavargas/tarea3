3ad5f4d97c2c65bcaba83f69f2bdcf4ed006a029
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:19:49 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/cmsocc/src/de/hybris/platform/cmsocc/helper/PageableData.java
@@ -0,0 +1,74 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN!
+ * --- Generated at Jul 24, 2022, 5:27:34 PM
+ * ----------------------------------------------------------------
+ *
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.cmsocc.helper;
+
+import java.io.Serializable;
+
+
+import java.util.Objects;
+/**
+ * POJO representation of search query pagination.
+ */
+public  class PageableData  implements Serializable 
+
+{
+
+	/** Default serialVersionUID value. */
+ 
+	private static final long serialVersionUID = 1L;
+
+	/** The number of results per page. A page may have less results if there are less than a full page of results, only on the last page in the results.<br/><br/><i>Generated property</i> for <code>PageableData.pageSize</code> property defined at extension <code>cms2</code>. */
+	
+	private int pageSize;
+
+	/** The current page number. The first page is number zero (0), the second page is number one (1), and so on.<br/><br/><i>Generated property</i> for <code>PageableData.currentPage</code> property defined at extension <code>cms2</code>. */
+	
+	private int currentPage;
+
+	/** The selected sort code.<br/><br/><i>Generated property</i> for <code>PageableData.sort</code> property defined at extension <code>cms2</code>. */
+	
+	private String sort;
+	
+	public PageableData()
+	{
+		// default constructor
+	}
+	
+	public void setPageSize(final int pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public int getPageSize() 
+	{
+		return pageSize;
+	}
+	
+	public void setCurrentPage(final int currentPage)
+	{
+		this.currentPage = currentPage;
+	}
+
+	public int getCurrentPage() 
+	{
+		return currentPage;
+	}
+	
+	public void setSort(final String sort)
+	{
+		this.sort = sort;
+	}
+
+	public String getSort() 
+	{
+		return sort;
+	}
+	
+
+}
