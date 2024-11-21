10edef459a9b4ef957049f75b286fc067be69b54
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:06 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/ErrorList.java
@@ -0,0 +1,25 @@
+package mx.com.telcel.models.cvtdat;
+
+public class ErrorList
+{
+
+	private ErrorDetailRes error;
+
+	/**
+	 * @return the error
+	 */
+	public ErrorDetailRes getError()
+	{
+		return error;
+	}
+
+	/**
+	 * @param error
+	 *           the error to set
+	 */
+	public void setError(final ErrorDetailRes error)
+	{
+		this.error = error;
+	}
+
+}
