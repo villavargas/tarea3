eb88bd986608cd4ca7c633a5908ef0c9f1cf2152
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:05:12 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/response/models/RevokeResponse.java
@@ -0,0 +1,42 @@
+package mx.com.telcel.response.models;
+
+public class RevokeResponse {
+
+    private String error;
+    private String error_description;
+
+    /**
+     * @return the error
+     */
+    public String getError()
+    {
+        return error;
+    }
+
+    /**
+     * @param error
+     *           the error to set
+     */
+    public void setError(final String error)
+    {
+        this.error = error;
+    }
+
+    /**
+     * @return the error_description
+     */
+    public String getError_description()
+    {
+        return error_description;
+    }
+
+    /**
+     * @param error_description
+     *           the error_description to set
+     */
+    public void setError_description(final String error_description)
+    {
+        this.error_description = error_description;
+    }
+
+}
