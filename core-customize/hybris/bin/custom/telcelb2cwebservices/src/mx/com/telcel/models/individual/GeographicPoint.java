e1f1c474c53db43b828516c2c1a6a77ab5ae956a
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:16:49 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/GeographicPoint.java
@@ -0,0 +1,61 @@
+package mx.com.telcel.models.individual;
+
+public class GeographicPoint
+{
+
+	private String x;
+	private String y;
+	private String z;
+
+	/**
+	 * @return the x
+	 */
+	public String getX()
+	{
+		return x;
+	}
+
+	/**
+	 * @param x
+	 *           the x to set
+	 */
+	public void setX(final String x)
+	{
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public String getY()
+	{
+		return y;
+	}
+
+	/**
+	 * @param y
+	 *           the y to set
+	 */
+	public void setY(final String y)
+	{
+		this.y = y;
+	}
+
+	/**
+	 * @return the z
+	 */
+	public String getZ()
+	{
+		return z;
+	}
+
+	/**
+	 * @param z
+	 *           the z to set
+	 */
+	public void setZ(final String z)
+	{
+		this.z = z;
+	}
+
+}
