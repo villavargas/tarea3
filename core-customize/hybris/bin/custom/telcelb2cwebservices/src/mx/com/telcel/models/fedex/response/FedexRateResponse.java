b027e9fbbc3c589a2c0ab6376291b94d21c82bfc
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:13:35 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/response/FedexRateResponse.java
@@ -0,0 +1,32 @@
+package mx.com.telcel.models.fedex.response;
+
+public class FedexRateResponse {
+
+    private int code;
+    private String description;
+    private String deliveryDate;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(String deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+}
