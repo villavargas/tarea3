da809f1a67e55845e90976d84a6e2085ca35e037
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:05:48 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/response/models/AuthorizationResponse.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.response.models;
+
+public class AuthorizationResponse
+{
+
+	private String code;
+	private String error;
+
+	/**
+	 * @return the code
+	 */
+	public String getCode()
+	{
+		return code;
+	}
+
+	/**
+	 * @param code
+	 *           the code to set
+	 */
+	public void setCode(final String code)
+	{
+		this.code = code;
+	}
+
+	/**
+	 * @return the error
+	 */
+	public String getError()
+	{
+		return error;
+	}
+
+	/**
+	 * @param error
+	 *           the error to set
+	 */
+	public void setError(final String error)
+	{
+		this.error = error;
+	}
+
+}
