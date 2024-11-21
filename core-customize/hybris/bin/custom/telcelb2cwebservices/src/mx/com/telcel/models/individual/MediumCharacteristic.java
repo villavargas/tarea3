e5d460eb2551c73a415ab46cddcab14dc5dcd8fa
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:02 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/MediumCharacteristic.java
@@ -0,0 +1,244 @@
+package mx.com.telcel.models.individual;
+
+import java.util.List;
+
+
+public class MediumCharacteristic
+{
+
+	private String city;
+	private String contactType;
+	private String country;
+	private String emailAddress;
+	private String faxNumber;
+	private String phoneNumber;
+	private String mobileNumber;
+	private String postCode;
+	private String webSite;
+	private String stateOrProvince;
+	private String street1;
+	private String street2;
+	private List<CalendarPeriod> calendar;
+
+	/**
+	 * @return the city
+	 */
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *           the city to set
+	 */
+	public void setCity(final String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @return the contactType
+	 */
+	public String getContactType()
+	{
+		return contactType;
+	}
+
+	/**
+	 * @param contactType
+	 *           the contactType to set
+	 */
+	public void setContactType(final String contactType)
+	{
+		this.contactType = contactType;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry()
+	{
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *           the country to set
+	 */
+	public void setCountry(final String country)
+	{
+		this.country = country;
+	}
+
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress()
+	{
+		return emailAddress;
+	}
+
+	/**
+	 * @param emailAddress
+	 *           the emailAddress to set
+	 */
+	public void setEmailAddress(final String emailAddress)
+	{
+		this.emailAddress = emailAddress;
+	}
+
+	/**
+	 * @return the faxNumber
+	 */
+	public String getFaxNumber()
+	{
+		return faxNumber;
+	}
+
+	/**
+	 * @param faxNumber
+	 *           the faxNumber to set
+	 */
+	public void setFaxNumber(final String faxNumber)
+	{
+		this.faxNumber = faxNumber;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *           the phoneNumber to set
+	 */
+	public void setPhoneNumber(final String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the mobileNumber
+	 */
+	public String getMobileNumber()
+	{
+		return mobileNumber;
+	}
+
+	/**
+	 * @param mobileNumber
+	 *           the mobileNumber to set
+	 */
+	public void setMobileNumber(final String mobileNumber)
+	{
+		this.mobileNumber = mobileNumber;
+	}
+
+	/**
+	 * @return the postCode
+	 */
+	public String getPostCode()
+	{
+		return postCode;
+	}
+
+	/**
+	 * @param postCode
+	 *           the postCode to set
+	 */
+	public void setPostCode(final String postCode)
+	{
+		this.postCode = postCode;
+	}
+
+	/**
+	 * @return the webSite
+	 */
+	public String getWebSite()
+	{
+		return webSite;
+	}
+
+	/**
+	 * @param webSite
+	 *           the webSite to set
+	 */
+	public void setWebSite(final String webSite)
+	{
+		this.webSite = webSite;
+	}
+
+	/**
+	 * @return the stateOrProvince
+	 */
+	public String getStateOrProvince()
+	{
+		return stateOrProvince;
+	}
+
+	/**
+	 * @param stateOrProvince
+	 *           the stateOrProvince to set
+	 */
+	public void setStateOrProvince(final String stateOrProvince)
+	{
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	/**
+	 * @return the street1
+	 */
+	public String getStreet1()
+	{
+		return street1;
+	}
+
+	/**
+	 * @param street1
+	 *           the street1 to set
+	 */
+	public void setStreet1(final String street1)
+	{
+		this.street1 = street1;
+	}
+
+	/**
+	 * @return the street2
+	 */
+	public String getStreet2()
+	{
+		return street2;
+	}
+
+	/**
+	 * @param street2
+	 *           the street2 to set
+	 */
+	public void setStreet2(final String street2)
+	{
+		this.street2 = street2;
+	}
+
+	/**
+	 * @return the calendar
+	 */
+	public List<CalendarPeriod> getCalendar()
+	{
+		return calendar;
+	}
+
+	/**
+	 * @param calendar
+	 *           the calendar to set
+	 */
+	public void setCalendar(final List<CalendarPeriod> calendar)
+	{
+		this.calendar = calendar;
+	}
+
+}
