e6d66a43df79e692e99d1bc53ce353c99c1ea1eb
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:25:02 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/GeographicSubAddress.java
@@ -0,0 +1,115 @@
+package mx.com.telcel.models.cvtdat;
+
+public class GeographicSubAddress
+{
+
+	private String type;
+	private String fullAddress;
+	private String id;
+	private String name;
+	private String href;
+	private String organizationType;
+
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *           the type to set
+	 */
+	public void setType(final String type)
+	{
+		this.type = type;
+	}
+
+	/**
+	 * @return the fullAddress
+	 */
+	public String getFullAddress()
+	{
+		return fullAddress;
+	}
+
+	/**
+	 * @param fullAddress
+	 *           the fullAddress to set
+	 */
+	public void setFullAddress(final String fullAddress)
+	{
+		this.fullAddress = fullAddress;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(final String id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the href
+	 */
+	public String getHref()
+	{
+		return href;
+	}
+
+	/**
+	 * @param href
+	 *           the href to set
+	 */
+	public void setHref(final String href)
+	{
+		this.href = href;
+	}
+
+	/**
+	 * @return the organizationType
+	 */
+	public String getOrganizationType()
+	{
+		return organizationType;
+	}
+
+	/**
+	 * @param organizationType
+	 *           the organizationType to set
+	 */
+	public void setOrganizationType(final String organizationType)
+	{
+		this.organizationType = organizationType;
+	}
+
+}
