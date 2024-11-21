d31d8663d1b4ef74c623f0808c7fa5c3f854521b
villavargas <villa_vargas@hotmail.com>
2024-11-21 06:25:55 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/Entitlement.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.cvtdat;
+
+public class Entitlement
+{
+
+	private String function;
+	private String id;
+
+	/**
+	 * @return the function
+	 */
+	public String getFunction()
+	{
+		return function;
+	}
+
+	/**
+	 * @param function
+	 *           the function to set
+	 */
+	public void setFunction(final String function)
+	{
+		this.function = function;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(final String id)
+	{
+		this.id = id;
+	}
+
+}
