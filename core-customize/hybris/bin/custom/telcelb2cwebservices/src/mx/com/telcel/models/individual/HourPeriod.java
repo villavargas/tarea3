7d4aeafa8eb8faf590351f95afdf89f495edbbee
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:05 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/HourPeriod.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.individual;
+
+public class HourPeriod
+{
+
+	private String endHour;
+	private String startHour;
+
+	/**
+	 * @return the endHour
+	 */
+	public String getEndHour()
+	{
+		return endHour;
+	}
+
+	/**
+	 * @param endHour
+	 *           the endHour to set
+	 */
+	public void setEndHour(final String endHour)
+	{
+		this.endHour = endHour;
+	}
+
+	/**
+	 * @return the startHour
+	 */
+	public String getStartHour()
+	{
+		return startHour;
+	}
+
+	/**
+	 * @param startHour
+	 *           the startHour to set
+	 */
+	public void setStartHour(final String startHour)
+	{
+		this.startHour = startHour;
+	}
+
+}
