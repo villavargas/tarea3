26f57dfb8dcf5e074ab5d40910f902105b6a01c8
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:17:23 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/Address.java
@@ -0,0 +1,277 @@
+package mx.com.telcel.models.fedex;
+
+public class Address
+{
+
+	private String streetName;
+	private String streetType;
+	private String zipCode;
+	private String locality;
+	private String block;
+	private String county;
+	private String city;
+	private String stateOrProvince;
+	private String country;
+	private String streetNumber;
+	private String streetNumberInt;
+	private String addressLine1;
+	private String addressLine2;
+	private String note;
+	private String isResidential;
+
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName()
+	{
+		return streetName;
+	}
+
+	/**
+	 * @param streetName
+	 *           the streetName to set
+	 */
+	public void setStreetName(final String streetName)
+	{
+		this.streetName = streetName;
+	}
+
+	/**
+	 * @return the streetType
+	 */
+	public String getStreetType()
+	{
+		return streetType;
+	}
+
+	/**
+	 * @param streetType
+	 *           the streetType to set
+	 */
+	public void setStreetType(final String streetType)
+	{
+		this.streetType = streetType;
+	}
+
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode()
+	{
+		return zipCode;
+	}
+
+	/**
+	 * @param zipCode
+	 *           the zipCode to set
+	 */
+	public void setZipCode(final String zipCode)
+	{
+		this.zipCode = zipCode;
+	}
+
+	/**
+	 * @return the locality
+	 */
+	public String getLocality()
+	{
+		return locality;
+	}
+
+	/**
+	 * @param locality
+	 *           the locality to set
+	 */
+	public void setLocality(final String locality)
+	{
+		this.locality = locality;
+	}
+
+	/**
+	 * @return the block
+	 */
+	public String getBlock()
+	{
+		return block;
+	}
+
+	/**
+	 * @param block
+	 *           the block to set
+	 */
+	public void setBlock(final String block)
+	{
+		this.block = block;
+	}
+
+	/**
+	 * @return the county
+	 */
+	public String getCounty()
+	{
+		return county;
+	}
+
+	/**
+	 * @param county
+	 *           the county to set
+	 */
+	public void setCounty(final String county)
+	{
+		this.county = county;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *           the city to set
+	 */
+	public void setCity(final String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @return the stateOrProvince
+	 */
+	public String getStateOrProvince()
+	{
+		return stateOrProvince;
+	}
+
+	/**
+	 * @param stateOrProvince
+	 *           the stateOrProvince to set
+	 */
+	public void setStateOrProvince(final String stateOrProvince)
+	{
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry()
+	{
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *           the country to set
+	 */
+	public void setCountry(final String country)
+	{
+		this.country = country;
+	}
+
+	/**
+	 * @return the streetNumber
+	 */
+	public String getStreetNumber()
+	{
+		return streetNumber;
+	}
+
+	/**
+	 * @param streetNumber
+	 *           the streetNumber to set
+	 */
+	public void setStreetNumber(final String streetNumber)
+	{
+		this.streetNumber = streetNumber;
+	}
+
+	/**
+	 * @return the streetNumberInt
+	 */
+	public String getStreetNumberInt()
+	{
+		return streetNumberInt;
+	}
+
+	/**
+	 * @param streetNumberInt
+	 *           the streetNumberInt to set
+	 */
+	public void setStreetNumberInt(final String streetNumberInt)
+	{
+		this.streetNumberInt = streetNumberInt;
+	}
+
+	/**
+	 * @return the addressLine1
+	 */
+	public String getAddressLine1()
+	{
+		return addressLine1;
+	}
+
+	/**
+	 * @param addressLine1
+	 *           the addressLine1 to set
+	 */
+	public void setAddressLine1(final String addressLine1)
+	{
+		this.addressLine1 = addressLine1;
+	}
+
+	/**
+	 * @return the addressLine2
+	 */
+	public String getAddressLine2()
+	{
+		return addressLine2;
+	}
+
+	/**
+	 * @param addressLine2
+	 *           the addressLine2 to set
+	 */
+	public void setAddressLine2(final String addressLine2)
+	{
+		this.addressLine2 = addressLine2;
+	}
+
+	/**
+	 * @return the note
+	 */
+	public String getNote()
+	{
+		return note;
+	}
+
+	/**
+	 * @param note
+	 *           the note to set
+	 */
+	public void setNote(final String note)
+	{
+		this.note = note;
+	}
+
+	/**
+	 * @return the isResidential
+	 */
+	public String getIsResidential()
+	{
+		return isResidential;
+	}
+
+	/**
+	 * @param isResidential
+	 *           the isResidential to set
+	 */
+	public void setIsResidential(final String isResidential)
+	{
+		this.isResidential = isResidential;
+	}
+
+}
