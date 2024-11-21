406bd69584b9cbcb003cb3842ad5dee67a4956e8
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:27:16 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfulfilmentprocess/testsrc/mx/com/telcel/fulfilmentprocess/test/actions/PrepareOrderForManualCheck.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.fulfilmentprocess.test.actions;
+
+/**
+ * Test counterpart for
+ * {@link mx.com.telcel.fulfilmentprocess.actions.order.PrepareOrderForManualCheckAction}
+ */
+public class PrepareOrderForManualCheck extends TestActionTemp
+{
+	//EMPTY
+}
