9f279451b1ab4d04f9fd1481085b75b52771ecb9
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:24:23 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2ccommercewebservices/src/mx/com/telcel/queues/impl/OrderStatusUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.queues.impl;
+
+import mx.com.telcel.queues.data.OrderStatusUpdateElementData;
+
+
+/**
+ * Queue for {@link mx.com.telcel.queues.data.OrderStatusUpdateElementData}
+ */
+public class OrderStatusUpdateQueue extends AbstractUpdateQueue<OrderStatusUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
