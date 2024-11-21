f85911252cc07a6c76f71d82d5d5849f74e3cf1a
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:10:14 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/UserInfo.java
@@ -0,0 +1,1144 @@
+package mx.com.telcel.models.cvtdat;
+
+import java.util.List;
+
+
+public class UserInfo
+{
+
+	private String sub;
+	private String externalId;
+	private String honorificPrefix;
+	private String honorificSuffix;
+	private String title;
+	private String name;
+	private String status;
+	private String givenName;
+	private String familyName;
+	private String secondLastName;
+	private String middleName;
+	private String nickname;
+	private String preferredUsername;
+	private String birthdate;
+	private String locale;
+	private String preferredLanguage;
+	private String zoneinfo;
+	private String profile;
+	private String picture;
+	private String website;
+	private String loginName;
+	private String stateDate;
+	private String creationDate;
+	private String creationSource;
+	private String lastUpdateDate;
+	private String lastUpdateSource;
+	private String lastAccessDate;
+	private String lockDate;
+	private Boolean registrationCompleted;
+	private String password;
+	private String state;
+	private String stateReason;
+	private String lastStartTime;
+	private List<Email> email;
+	private Phone phone;
+	private List<GeographicAddress> address;
+	private List<Characteristic> characteristic;
+	private List<RelatedParty> relatedParty;
+	private ValidFor validFor;
+	private Identification legalId;
+	private List<UserAsset> userAssets;
+	private Contact contact;
+	private OrganizationRef organizationRef;
+	private Questions questions;
+	private Im im;
+	private String streetNr;
+	private String streetNrSuffix;
+	private String streetNrLast;
+	private String streetNrLastSuffix;
+	private String streetName;
+	private String streetType;
+	private String streetSuffix;
+	private String fullAddress;
+	private String postcode;
+	private String locality;
+	private String city;
+	private String stateOrProvince;
+	private String country;
+	private Boolean isPrimary;
+	private String formatted;
+	private String region;
+	private GeographicLocationRefOrValue geographicLocation;
+	private GeographicSubAddress geographicSubAddress;
+
+	/**
+	 * @return the sub
+	 */
+	public String getSub()
+	{
+		return sub;
+	}
+
+	/**
+	 * @param sub
+	 *           the sub to set
+	 */
+	public void setSub(final String sub)
+	{
+		this.sub = sub;
+	}
+
+	/**
+	 * @return the externalId
+	 */
+	public String getExternalId()
+	{
+		return externalId;
+	}
+
+	/**
+	 * @param externalId
+	 *           the externalId to set
+	 */
+	public void setExternalId(final String externalId)
+	{
+		this.externalId = externalId;
+	}
+
+	/**
+	 * @return the honorificPrefix
+	 */
+	public String getHonorificPrefix()
+	{
+		return honorificPrefix;
+	}
+
+	/**
+	 * @param honorificPrefix
+	 *           the honorificPrefix to set
+	 */
+	public void setHonorificPrefix(final String honorificPrefix)
+	{
+		this.honorificPrefix = honorificPrefix;
+	}
+
+	/**
+	 * @return the honorificSuffix
+	 */
+	public String getHonorificSuffix()
+	{
+		return honorificSuffix;
+	}
+
+	/**
+	 * @param honorificSuffix
+	 *           the honorificSuffix to set
+	 */
+	public void setHonorificSuffix(final String honorificSuffix)
+	{
+		this.honorificSuffix = honorificSuffix;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle()
+	{
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *           the title to set
+	 */
+	public void setTitle(final String title)
+	{
+		this.title = title;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus()
+	{
+		return status;
+	}
+
+	/**
+	 * @param status
+	 *           the status to set
+	 */
+	public void setStatus(final String status)
+	{
+		this.status = status;
+	}
+
+	/**
+	 * @return the givenName
+	 */
+	public String getGivenName()
+	{
+		return givenName;
+	}
+
+	/**
+	 * @param givenName
+	 *           the givenName to set
+	 */
+	public void setGivenName(final String givenName)
+	{
+		this.givenName = givenName;
+	}
+
+	/**
+	 * @return the familyName
+	 */
+	public String getFamilyName()
+	{
+		return familyName;
+	}
+
+	/**
+	 * @param familyName
+	 *           the familyName to set
+	 */
+	public void setFamilyName(final String familyName)
+	{
+		this.familyName = familyName;
+	}
+
+	/**
+	 * @return the secondLastName
+	 */
+	public String getSecondLastName()
+	{
+		return secondLastName;
+	}
+
+	/**
+	 * @param secondLastName
+	 *           the secondLastName to set
+	 */
+	public void setSecondLastName(final String secondLastName)
+	{
+		this.secondLastName = secondLastName;
+	}
+
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName()
+	{
+		return middleName;
+	}
+
+	/**
+	 * @param middleName
+	 *           the middleName to set
+	 */
+	public void setMiddleName(final String middleName)
+	{
+		this.middleName = middleName;
+	}
+
+	/**
+	 * @return the nickname
+	 */
+	public String getNickname()
+	{
+		return nickname;
+	}
+
+	/**
+	 * @param nickname
+	 *           the nickname to set
+	 */
+	public void setNickname(final String nickname)
+	{
+		this.nickname = nickname;
+	}
+
+	/**
+	 * @return the preferredUsername
+	 */
+	public String getPreferredUsername()
+	{
+		return preferredUsername;
+	}
+
+	/**
+	 * @param preferredUsername
+	 *           the preferredUsername to set
+	 */
+	public void setPreferredUsername(final String preferredUsername)
+	{
+		this.preferredUsername = preferredUsername;
+	}
+
+	/**
+	 * @return the birthdate
+	 */
+	public String getBirthdate()
+	{
+		return birthdate;
+	}
+
+	/**
+	 * @param birthdate
+	 *           the birthdate to set
+	 */
+	public void setBirthdate(final String birthdate)
+	{
+		this.birthdate = birthdate;
+	}
+
+	/**
+	 * @return the locale
+	 */
+	public String getLocale()
+	{
+		return locale;
+	}
+
+	/**
+	 * @param locale
+	 *           the locale to set
+	 */
+	public void setLocale(final String locale)
+	{
+		this.locale = locale;
+	}
+
+	/**
+	 * @return the preferredLanguage
+	 */
+	public String getPreferredLanguage()
+	{
+		return preferredLanguage;
+	}
+
+	/**
+	 * @param preferredLanguage
+	 *           the preferredLanguage to set
+	 */
+	public void setPreferredLanguage(final String preferredLanguage)
+	{
+		this.preferredLanguage = preferredLanguage;
+	}
+
+	/**
+	 * @return the zoneinfo
+	 */
+	public String getZoneinfo()
+	{
+		return zoneinfo;
+	}
+
+	/**
+	 * @param zoneinfo
+	 *           the zoneinfo to set
+	 */
+	public void setZoneinfo(final String zoneinfo)
+	{
+		this.zoneinfo = zoneinfo;
+	}
+
+	/**
+	 * @return the profile
+	 */
+	public String getProfile()
+	{
+		return profile;
+	}
+
+	/**
+	 * @param profile
+	 *           the profile to set
+	 */
+	public void setProfile(final String profile)
+	{
+		this.profile = profile;
+	}
+
+	/**
+	 * @return the picture
+	 */
+	public String getPicture()
+	{
+		return picture;
+	}
+
+	/**
+	 * @param picture
+	 *           the picture to set
+	 */
+	public void setPicture(final String picture)
+	{
+		this.picture = picture;
+	}
+
+	/**
+	 * @return the website
+	 */
+	public String getWebsite()
+	{
+		return website;
+	}
+
+	/**
+	 * @param website
+	 *           the website to set
+	 */
+	public void setWebsite(final String website)
+	{
+		this.website = website;
+	}
+
+	/**
+	 * @return the loginName
+	 */
+	public String getLoginName()
+	{
+		return loginName;
+	}
+
+	/**
+	 * @param loginName
+	 *           the loginName to set
+	 */
+	public void setLoginName(final String loginName)
+	{
+		this.loginName = loginName;
+	}
+
+	/**
+	 * @return the stateDate
+	 */
+	public String getStateDate()
+	{
+		return stateDate;
+	}
+
+	/**
+	 * @param stateDate
+	 *           the stateDate to set
+	 */
+	public void setStateDate(final String stateDate)
+	{
+		this.stateDate = stateDate;
+	}
+
+	/**
+	 * @return the creationDate
+	 */
+	public String getCreationDate()
+	{
+		return creationDate;
+	}
+
+	/**
+	 * @param creationDate
+	 *           the creationDate to set
+	 */
+	public void setCreationDate(final String creationDate)
+	{
+		this.creationDate = creationDate;
+	}
+
+	/**
+	 * @return the creationSource
+	 */
+	public String getCreationSource()
+	{
+		return creationSource;
+	}
+
+	/**
+	 * @param creationSource
+	 *           the creationSource to set
+	 */
+	public void setCreationSource(final String creationSource)
+	{
+		this.creationSource = creationSource;
+	}
+
+	/**
+	 * @return the lastUpdateDate
+	 */
+	public String getLastUpdateDate()
+	{
+		return lastUpdateDate;
+	}
+
+	/**
+	 * @param lastUpdateDate
+	 *           the lastUpdateDate to set
+	 */
+	public void setLastUpdateDate(final String lastUpdateDate)
+	{
+		this.lastUpdateDate = lastUpdateDate;
+	}
+
+	/**
+	 * @return the lastUpdateSource
+	 */
+	public String getLastUpdateSource()
+	{
+		return lastUpdateSource;
+	}
+
+	/**
+	 * @param lastUpdateSource
+	 *           the lastUpdateSource to set
+	 */
+	public void setLastUpdateSource(final String lastUpdateSource)
+	{
+		this.lastUpdateSource = lastUpdateSource;
+	}
+
+	/**
+	 * @return the lastAccessDate
+	 */
+	public String getLastAccessDate()
+	{
+		return lastAccessDate;
+	}
+
+	/**
+	 * @param lastAccessDate
+	 *           the lastAccessDate to set
+	 */
+	public void setLastAccessDate(final String lastAccessDate)
+	{
+		this.lastAccessDate = lastAccessDate;
+	}
+
+	/**
+	 * @return the lockDate
+	 */
+	public String getLockDate()
+	{
+		return lockDate;
+	}
+
+	/**
+	 * @param lockDate
+	 *           the lockDate to set
+	 */
+	public void setLockDate(final String lockDate)
+	{
+		this.lockDate = lockDate;
+	}
+
+	/**
+	 * @return the registrationCompleted
+	 */
+	public Boolean getRegistrationCompleted()
+	{
+		return registrationCompleted;
+	}
+
+	/**
+	 * @param registrationCompleted
+	 *           the registrationCompleted to set
+	 */
+	public void setRegistrationCompleted(final Boolean registrationCompleted)
+	{
+		this.registrationCompleted = registrationCompleted;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword()
+	{
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *           the password to set
+	 */
+	public void setPassword(final String password)
+	{
+		this.password = password;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState()
+	{
+		return state;
+	}
+
+	/**
+	 * @param state
+	 *           the state to set
+	 */
+	public void setState(final String state)
+	{
+		this.state = state;
+	}
+
+	/**
+	 * @return the stateReason
+	 */
+	public String getStateReason()
+	{
+		return stateReason;
+	}
+
+	/**
+	 * @param stateReason
+	 *           the stateReason to set
+	 */
+	public void setStateReason(final String stateReason)
+	{
+		this.stateReason = stateReason;
+	}
+
+	/**
+	 * @return the lastStartTime
+	 */
+	public String getLastStartTime()
+	{
+		return lastStartTime;
+	}
+
+	/**
+	 * @param lastStartTime
+	 *           the lastStartTime to set
+	 */
+	public void setLastStartTime(final String lastStartTime)
+	{
+		this.lastStartTime = lastStartTime;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public List<Email> getEmail()
+	{
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *           the email to set
+	 */
+	public void setEmail(final List<Email> email)
+	{
+		this.email = email;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public Phone getPhone()
+	{
+		return phone;
+	}
+
+	/**
+	 * @param phone
+	 *           the phone to set
+	 */
+	public void setPhone(final Phone phone)
+	{
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public List<GeographicAddress> getAddress()
+	{
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *           the address to set
+	 */
+	public void setAddress(final List<GeographicAddress> address)
+	{
+		this.address = address;
+	}
+
+	/**
+	 * @return the characteristic
+	 */
+	public List<Characteristic> getCharacteristic()
+	{
+		return characteristic;
+	}
+
+	/**
+	 * @param characteristic
+	 *           the characteristic to set
+	 */
+	public void setCharacteristic(final List<Characteristic> characteristic)
+	{
+		this.characteristic = characteristic;
+	}
+
+	/**
+	 * @return the relatedParty
+	 */
+	public List<RelatedParty> getRelatedParty()
+	{
+		return relatedParty;
+	}
+
+	/**
+	 * @param relatedParty
+	 *           the relatedParty to set
+	 */
+	public void setRelatedParty(final List<RelatedParty> relatedParty)
+	{
+		this.relatedParty = relatedParty;
+	}
+
+	/**
+	 * @return the validFor
+	 */
+	public ValidFor getValidFor()
+	{
+		return validFor;
+	}
+
+	/**
+	 * @param validFor
+	 *           the validFor to set
+	 */
+	public void setValidFor(final ValidFor validFor)
+	{
+		this.validFor = validFor;
+	}
+
+	/**
+	 * @return the legalId
+	 */
+	public Identification getLegalId()
+	{
+		return legalId;
+	}
+
+	/**
+	 * @param legalId
+	 *           the legalId to set
+	 */
+	public void setLegalId(final Identification legalId)
+	{
+		this.legalId = legalId;
+	}
+
+	/**
+	 * @return the userAssets
+	 */
+	public List<UserAsset> getUserAssets()
+	{
+		return userAssets;
+	}
+
+	/**
+	 * @param userAssets
+	 *           the userAssets to set
+	 */
+	public void setUserAssets(final List<UserAsset> userAssets)
+	{
+		this.userAssets = userAssets;
+	}
+
+	/**
+	 * @return the contact
+	 */
+	public Contact getContact()
+	{
+		return contact;
+	}
+
+	/**
+	 * @param contact
+	 *           the contact to set
+	 */
+	public void setContact(final Contact contact)
+	{
+		this.contact = contact;
+	}
+
+	/**
+	 * @return the organizationRef
+	 */
+	public OrganizationRef getOrganizationRef()
+	{
+		return organizationRef;
+	}
+
+	/**
+	 * @param organizationRef
+	 *           the organizationRef to set
+	 */
+	public void setOrganizationRef(final OrganizationRef organizationRef)
+	{
+		this.organizationRef = organizationRef;
+	}
+
+	/**
+	 * @return the questions
+	 */
+	public Questions getQuestions()
+	{
+		return questions;
+	}
+
+	/**
+	 * @param questions
+	 *           the questions to set
+	 */
+	public void setQuestions(final Questions questions)
+	{
+		this.questions = questions;
+	}
+
+	/**
+	 * @return the im
+	 */
+	public Im getIm()
+	{
+		return im;
+	}
+
+	/**
+	 * @param im
+	 *           the im to set
+	 */
+	public void setIm(final Im im)
+	{
+		this.im = im;
+	}
+
+	/**
+	 * @return the streetNr
+	 */
+	public String getStreetNr()
+	{
+		return streetNr;
+	}
+
+	/**
+	 * @param streetNr
+	 *           the streetNr to set
+	 */
+	public void setStreetNr(final String streetNr)
+	{
+		this.streetNr = streetNr;
+	}
+
+	/**
+	 * @return the streetNrSuffix
+	 */
+	public String getStreetNrSuffix()
+	{
+		return streetNrSuffix;
+	}
+
+	/**
+	 * @param streetNrSuffix
+	 *           the streetNrSuffix to set
+	 */
+	public void setStreetNrSuffix(final String streetNrSuffix)
+	{
+		this.streetNrSuffix = streetNrSuffix;
+	}
+
+	/**
+	 * @return the streetNrLast
+	 */
+	public String getStreetNrLast()
+	{
+		return streetNrLast;
+	}
+
+	/**
+	 * @param streetNrLast
+	 *           the streetNrLast to set
+	 */
+	public void setStreetNrLast(final String streetNrLast)
+	{
+		this.streetNrLast = streetNrLast;
+	}
+
+	/**
+	 * @return the streetNrLastSuffix
+	 */
+	public String getStreetNrLastSuffix()
+	{
+		return streetNrLastSuffix;
+	}
+
+	/**
+	 * @param streetNrLastSuffix
+	 *           the streetNrLastSuffix to set
+	 */
+	public void setStreetNrLastSuffix(final String streetNrLastSuffix)
+	{
+		this.streetNrLastSuffix = streetNrLastSuffix;
+	}
+
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName()
+	{
+		return streetName;
+	}
+
+	/**
+	 * @param streetName
+	 *           the streetName to set
+	 */
+	public void setStreetName(final String streetName)
+	{
+		this.streetName = streetName;
+	}
+
+	/**
+	 * @return the streetType
+	 */
+	public String getStreetType()
+	{
+		return streetType;
+	}
+
+	/**
+	 * @param streetType
+	 *           the streetType to set
+	 */
+	public void setStreetType(final String streetType)
+	{
+		this.streetType = streetType;
+	}
+
+	/**
+	 * @return the streetSuffix
+	 */
+	public String getStreetSuffix()
+	{
+		return streetSuffix;
+	}
+
+	/**
+	 * @param streetSuffix
+	 *           the streetSuffix to set
+	 */
+	public void setStreetSuffix(final String streetSuffix)
+	{
+		this.streetSuffix = streetSuffix;
+	}
+
+	/**
+	 * @return the fullAddress
+	 */
+	public String getFullAddress()
+	{
+		return fullAddress;
+	}
+
+	/**
+	 * @param fullAddress
+	 *           the fullAddress to set
+	 */
+	public void setFullAddress(final String fullAddress)
+	{
+		this.fullAddress = fullAddress;
+	}
+
+	/**
+	 * @return the postcode
+	 */
+	public String getPostcode()
+	{
+		return postcode;
+	}
+
+	/**
+	 * @param postcode
+	 *           the postcode to set
+	 */
+	public void setPostcode(final String postcode)
+	{
+		this.postcode = postcode;
+	}
+
+	/**
+	 * @return the locality
+	 */
+	public String getLocality()
+	{
+		return locality;
+	}
+
+	/**
+	 * @param locality
+	 *           the locality to set
+	 */
+	public void setLocality(final String locality)
+	{
+		this.locality = locality;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *           the city to set
+	 */
+	public void setCity(final String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @return the stateOrProvince
+	 */
+	public String getStateOrProvince()
+	{
+		return stateOrProvince;
+	}
+
+	/**
+	 * @param stateOrProvince
+	 *           the stateOrProvince to set
+	 */
+	public void setStateOrProvince(final String stateOrProvince)
+	{
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry()
+	{
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *           the country to set
+	 */
+	public void setCountry(final String country)
+	{
+		this.country = country;
+	}
+
+	/**
+	 * @return the isPrimary
+	 */
+	public Boolean getIsPrimary()
+	{
+		return isPrimary;
+	}
+
+	/**
+	 * @param isPrimary
+	 *           the isPrimary to set
+	 */
+	public void setIsPrimary(final Boolean isPrimary)
+	{
+		this.isPrimary = isPrimary;
+	}
+
+	/**
+	 * @return the formatted
+	 */
+	public String getFormatted()
+	{
+		return formatted;
+	}
+
+	/**
+	 * @param formatted
+	 *           the formatted to set
+	 */
+	public void setFormatted(final String formatted)
+	{
+		this.formatted = formatted;
+	}
+
+	/**
+	 * @return the region
+	 */
+	public String getRegion()
+	{
+		return region;
+	}
+
+	/**
+	 * @param region
+	 *           the region to set
+	 */
+	public void setRegion(final String region)
+	{
+		this.region = region;
+	}
+
+	/**
+	 * @return the geographicLocation
+	 */
+	public GeographicLocationRefOrValue getGeographicLocation()
+	{
+		return geographicLocation;
+	}
+
+	/**
+	 * @param geographicLocation
+	 *           the geographicLocation to set
+	 */
+	public void setGeographicLocation(final GeographicLocationRefOrValue geographicLocation)
+	{
+		this.geographicLocation = geographicLocation;
+	}
+
+	/**
+	 * @return the geographicSubAddress
+	 */
+	public GeographicSubAddress getGeographicSubAddress()
+	{
+		return geographicSubAddress;
+	}
+
+	/**
+	 * @param geographicSubAddress
+	 *           the geographicSubAddress to set
+	 */
+	public void setGeographicSubAddress(final GeographicSubAddress geographicSubAddress)
+	{
+		this.geographicSubAddress = geographicSubAddress;
+	}
+
+}
