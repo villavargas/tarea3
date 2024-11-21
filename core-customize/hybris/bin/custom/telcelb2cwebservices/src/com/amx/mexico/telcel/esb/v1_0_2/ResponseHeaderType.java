69f1817e52fd88307bf4a9bb1922627c0b2f1710
villavargas <villa_vargas@hotmail.com>
2024-11-20 22:34:21 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/mexico/telcel/esb/v1_0_2/ResponseHeaderType.java
@@ -0,0 +1,174 @@
+
+package com.amx.mexico.telcel.esb.v1_0_2;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ResponseHeaderType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ResponseHeaderType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="MessageUUID" type="{http://amx.com/mexico/telcel/esb/v1_0_2}MessageIDType"/&gt;
+ *         &lt;element name="TimeStamp" type="{http://amx.com/mexico/telcel/esb/v1_0_2}TimeStampFormat"/&gt;
+ *         &lt;element name="SendBy" type="{http://amx.com/mexico/telcel/esb/v1_0_2}SenderType"/&gt;
+ *         &lt;element name="Latency" type="{http://amx.com/mexico/telcel/esb/v1_0_2}LatencyType"/&gt;
+ *         &lt;element name="Version" type="{http://amx.com/mexico/telcel/esb/v1_0_2}VersionType"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ResponseHeaderType", propOrder = {
+    "messageUUID",
+    "timeStamp",
+    "sendBy",
+    "latency",
+    "version"
+})
+public class ResponseHeaderType {
+
+    @XmlElement(name = "MessageUUID", required = true)
+    protected String messageUUID;
+    @XmlElement(name = "TimeStamp", required = true)
+    protected String timeStamp;
+    @XmlElement(name = "SendBy", required = true)
+    protected String sendBy;
+    @XmlElement(name = "Latency", required = true)
+    protected String latency;
+    @XmlElement(name = "Version", required = true)
+    protected String version;
+
+    /**
+     * Gets the value of the messageUUID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMessageUUID() {
+        return messageUUID;
+    }
+
+    /**
+     * Sets the value of the messageUUID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMessageUUID(String value) {
+        this.messageUUID = value;
+    }
+
+    /**
+     * Gets the value of the timeStamp property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    /**
+     * Sets the value of the timeStamp property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTimeStamp(String value) {
+        this.timeStamp = value;
+    }
+
+    /**
+     * Gets the value of the sendBy property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSendBy() {
+        return sendBy;
+    }
+
+    /**
+     * Sets the value of the sendBy property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSendBy(String value) {
+        this.sendBy = value;
+    }
+
+    /**
+     * Gets the value of the latency property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLatency() {
+        return latency;
+    }
+
+    /**
+     * Sets the value of the latency property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLatency(String value) {
+        this.latency = value;
+    }
+
+    /**
+     * Gets the value of the version property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the value of the version property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setVersion(String value) {
+        this.version = value;
+    }
+
+}
