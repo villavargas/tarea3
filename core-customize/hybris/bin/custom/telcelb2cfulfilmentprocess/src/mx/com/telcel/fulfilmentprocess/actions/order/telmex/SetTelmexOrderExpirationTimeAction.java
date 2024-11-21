16d2bfc6fa98f5610faeed89204001667cf2bb9e
villavargas <villa_vargas@hotmail.com>
2024-11-20 22:34:57 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfulfilmentprocess/src/mx/com/telcel/fulfilmentprocess/actions/order/telmex/SetTelmexOrderExpirationTimeAction.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.fulfilmentprocess.actions.order.telmex;
+
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.orderprocessing.model.OrderProcessModel;
+import de.hybris.platform.processengine.action.AbstractProceduralAction;
+import de.hybris.platform.servicelayer.time.TimeService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * The type Set telmex order expiration time action.
+ */
+public class SetTelmexOrderExpirationTimeAction extends AbstractProceduralAction<OrderProcessModel> {
+    private static final Logger LOG = Logger.getLogger(SetTelmexOrderExpirationTimeAction.class);
+
+    private TimeService timeService;
+
+    @Override
+    public void executeAction(final OrderProcessModel process) {
+        if (LOG.isInfoEnabled()) {
+            LOG.info("Process: " + process.getCode() + " in step " + getClass());
+        }
+        final OrderModel order = process.getOrder();
+        order.setExpirationTime(getTimeService().getCurrentTime());
+        getModelService().save(order);
+    }
+
+    /**
+     * Gets time service.
+     *
+     * @return the time service
+     */
+    protected TimeService getTimeService() {
+        return timeService;
+    }
+
+    /**
+     * Sets time service.
+     *
+     * @param timeService the time service
+     */
+    @Required
+    public void setTimeService(final TimeService timeService) {
+        this.timeService = timeService;
+    }
+}
