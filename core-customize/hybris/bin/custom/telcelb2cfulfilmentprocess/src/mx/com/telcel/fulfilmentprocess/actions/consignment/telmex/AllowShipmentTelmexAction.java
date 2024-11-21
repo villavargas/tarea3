14e368b3c75395d25edada90d81cae6f76458cc7
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:23:08 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfulfilmentprocess/src/mx/com/telcel/fulfilmentprocess/actions/consignment/telmex/AllowShipmentTelmexAction.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.fulfilmentprocess.actions.consignment.telmex;
+
+import de.hybris.platform.commerceservices.model.PickUpDeliveryModeModel;
+import de.hybris.platform.core.enums.OrderStatus;
+import de.hybris.platform.ordersplitting.model.ConsignmentModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentProcessModel;
+import de.hybris.platform.processengine.action.AbstractAction;
+import de.hybris.platform.warehouse.Process2WarehouseAdapter;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The type Allow shipment telmex action.
+ */
+public class AllowShipmentTelmexAction extends AbstractAction<ConsignmentProcessModel> {
+    private static final Logger LOG = Logger.getLogger(AllowShipmentTelmexAction.class);
+
+    private Process2WarehouseAdapter process2WarehouseAdapter;
+
+    /**
+     * The enum Transition.
+     */
+    public enum Transition {
+        /**
+         * Delivery transition.
+         */
+        DELIVERY,
+        /**
+         * Pickup transition.
+         */
+        PICKUP,
+        /**
+         * Cancel transition.
+         */
+        CANCEL,
+        /**
+         * Error transition.
+         */
+        ERROR;
+
+        /**
+         * Gets string values.
+         *
+         * @return the string values
+         */
+        public static Set<String> getStringValues() {
+            final Set<String> res = new HashSet<String>();
+
+            for (final Transition transition : Transition.values()) {
+                res.add(transition.toString());
+            }
+            return res;
+        }
+    }
+
+    @Override
+    public String execute(final ConsignmentProcessModel process) {
+        final ConsignmentModel consignment = process.getConsignment();
+        if (consignment != null) {
+            try {
+                // Check if the Order is Cancelled
+                if (OrderStatus.CANCELLED.equals(consignment.getOrder().getStatus())
+                        || OrderStatus.CANCELLING.equals(consignment.getOrder().getStatus())) {
+                    return Transition.CANCEL.toString();
+                } else {
+                    getProcess2WarehouseAdapter().shipConsignment(process.getConsignment());
+                    return getTransitionForConsignment(consignment);
+                }
+            } catch (final Exception e) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug(e);
+                }
+                return Transition.ERROR.toString();
+            }
+        }
+        return Transition.ERROR.toString();
+    }
+
+    /**
+     * Gets transition for consignment.
+     *
+     * @param consignment the consignment
+     * @return the transition for consignment
+     */
+    protected String getTransitionForConsignment(final ConsignmentModel consignment) {
+        if (consignment.getDeliveryMode() instanceof PickUpDeliveryModeModel) {
+            return Transition.PICKUP.toString();
+        } else {
+            return Transition.DELIVERY.toString();
+        }
+    }
+
+    /**
+     * Gets process 2 warehouse adapter.
+     *
+     * @return the process 2 warehouse adapter
+     */
+    protected Process2WarehouseAdapter getProcess2WarehouseAdapter() {
+        return process2WarehouseAdapter;
+    }
+
+    /**
+     * Sets process 2 warehouse adapter.
+     *
+     * @param process2WarehouseAdapter the process 2 warehouse adapter
+     */
+    @Required
+    public void setProcess2WarehouseAdapter(final Process2WarehouseAdapter process2WarehouseAdapter) {
+        this.process2WarehouseAdapter = process2WarehouseAdapter;
+    }
+
+    @Override
+    public Set<String> getTransitions() {
+        return Transition.getStringValues();
+    }
+}
