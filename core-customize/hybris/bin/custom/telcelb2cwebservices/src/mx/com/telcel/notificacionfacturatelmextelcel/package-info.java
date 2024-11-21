d42f7062cd9cd6043afc64fcde2fc0a73e742955
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:18:12 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/notificacionfacturatelmextelcel/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ibcomercio/telcel.com/commerce/services/SAP/NotificacionFacturaTelmex/")
+package mx.com.telcel.notificacionfacturatelmextelcel;
