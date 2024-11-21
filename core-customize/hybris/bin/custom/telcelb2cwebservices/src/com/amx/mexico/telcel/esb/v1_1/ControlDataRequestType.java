98ac1c6bd41cef765633363368eedadc07ec7400
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:07:11 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/mexico/telcel/esb/v1_1/ControlDataRequestType.java
@@ -0,0 +1,230 @@
+
+package com.amx.mexico.telcel.esb.v1_1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for ControlDataRequestType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ControlDataRequestType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="messageUUID" type="{http://amx.com/mexico/telcel/esb/v1_1}MessageUUIDType"/&gt;
+ *         &lt;element name="requestDate" type="{http://amx.com/mexico/telcel/esb/v1_1}DateTimeType"/&gt;
+ *         &lt;element name="sendBy" type="{http://amx.com/mexico/telcel/esb/v1_1}SenderType"/&gt;
+ *         &lt;element name="version" type="{http://amx.com/mexico/telcel/esb/v1_1}VersionType"/&gt;
+ *         &lt;element name="ipClient" type="{http://amx.com/mexico/telcel/esb/v1_1}IPv4AddressType" minOccurs="0"/&gt;
+ *         &lt;element name="ipServer" type="{http://amx.com/mexico/telcel/esb/v1_1}IPv4AddressType" minOccurs="0"/&gt;
+ *         &lt;element name="user" type="{http://amx.com/mexico/telcel/esb/v1_1}UserType" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ControlDataRequestType", propOrder = {
+    "messageUUID",
+    "requestDate",
+    "sendBy",
+    "version",
+    "ipClient",
+    "ipServer",
+    "user"
+})
+public class ControlDataRequestType {
+
+    @XmlElement(required = true)
+    protected String messageUUID;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar requestDate;
+    @XmlElement(required = true)
+    protected String sendBy;
+    @XmlElement(required = true)
+    protected String version;
+    protected String ipClient;
+    protected String ipServer;
+    protected String user;
+
+    /**
+     * Gets the value of the messageUUID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMessageUUID() {
+        return messageUUID;
+    }
+
+    /**
+     * Sets the value of the messageUUID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMessageUUID(String value) {
+        this.messageUUID = value;
+    }
+
+    /**
+     * Gets the value of the requestDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getRequestDate() {
+        return requestDate;
+    }
+
+    /**
+     * Sets the value of the requestDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setRequestDate(XMLGregorianCalendar value) {
+        this.requestDate = value;
+    }
+
+    /**
+     * Gets the value of the sendBy property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSendBy() {
+        return sendBy;
+    }
+
+    /**
+     * Sets the value of the sendBy property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSendBy(String value) {
+        this.sendBy = value;
+    }
+
+    /**
+     * Gets the value of the version property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the value of the version property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setVersion(String value) {
+        this.version = value;
+    }
+
+    /**
+     * Gets the value of the ipClient property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIpClient() {
+        return ipClient;
+    }
+
+    /**
+     * Sets the value of the ipClient property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIpClient(String value) {
+        this.ipClient = value;
+    }
+
+    /**
+     * Gets the value of the ipServer property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIpServer() {
+        return ipServer;
+    }
+
+    /**
+     * Sets the value of the ipServer property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIpServer(String value) {
+        this.ipServer = value;
+    }
+
+    /**
+     * Gets the value of the user property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the value of the user property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUser(String value) {
+        this.user = value;
+    }
+
+}
