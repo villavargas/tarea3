580c00e4b0b54b5a0728e3b7a8a41ae55606d0ce
villavargas <villa_vargas@hotmail.com>
2024-11-20 22:37:00 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/ErrorComponent.java
@@ -0,0 +1,25 @@
+package mx.com.telcel.models.fedex;
+
+public class ErrorComponent
+{
+
+	private mx.com.telcel.models.fedex.Error error;
+
+	/**
+	 * @return the error
+	 */
+	public mx.com.telcel.models.fedex.Error getError()
+	{
+		return error;
+	}
+
+	/**
+	 * @param error
+	 *           the error to set
+	 */
+	public void setError(final mx.com.telcel.models.fedex.Error error)
+	{
+		this.error = error;
+	}
+
+}
