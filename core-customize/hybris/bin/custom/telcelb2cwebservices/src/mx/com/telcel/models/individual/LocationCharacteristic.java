b21f1680c7f2890aa2aea06e79d3fd8334d77dcc
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:06:09 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/LocationCharacteristic.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.individual;
+
+public class LocationCharacteristic
+{
+
+	private String name;
+	private String value;
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *           the value to set
+	 */
+	public void setValue(final String value)
+	{
+		this.value = value;
+	}
+
+}
