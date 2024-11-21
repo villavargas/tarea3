4c6f129e9431391f833aa1b72fbe8a18df6d447c
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:12:17 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/web/src/mx/com/telcel/Telcelb2cwebservicesWebHelper.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Simple test class to demonstrate how to include utility classes to your webmodule.
+ */
+public class Telcelb2cwebservicesWebHelper
+{
+	/** Edit the local|project.properties to change logging behavior (properties log4j.*). */
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger(Telcelb2cwebservicesWebHelper.class.getName());
+
+	private Telcelb2cwebservicesWebHelper()
+	{
+	}
+
+	public static final String getTestOutput()
+	{
+		return "testoutput";
+	}
+}
