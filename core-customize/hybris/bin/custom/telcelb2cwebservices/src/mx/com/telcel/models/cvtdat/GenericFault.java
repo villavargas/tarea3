91cb3af1ab79bcfca0d9df73187ee810e4ab5bbe
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:25:05 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/GenericFault.java
@@ -0,0 +1,82 @@
+package mx.com.telcel.models.cvtdat;
+
+import java.util.List;
+
+
+public class GenericFault
+{
+
+	private String messageUUID;
+	private String responseDate;
+	private String latency;
+	private List<ErrorList> errorList;
+
+	/**
+	 * @return the messageUUID
+	 */
+	public String getMessageUUID()
+	{
+		return messageUUID;
+	}
+
+	/**
+	 * @param messageUUID
+	 *           the messageUUID to set
+	 */
+	public void setMessageUUID(final String messageUUID)
+	{
+		this.messageUUID = messageUUID;
+	}
+
+	/**
+	 * @return the responseDate
+	 */
+	public String getResponseDate()
+	{
+		return responseDate;
+	}
+
+	/**
+	 * @param responseDate
+	 *           the responseDate to set
+	 */
+	public void setResponseDate(final String responseDate)
+	{
+		this.responseDate = responseDate;
+	}
+
+	/**
+	 * @return the latency
+	 */
+	public String getLatency()
+	{
+		return latency;
+	}
+
+	/**
+	 * @param latency
+	 *           the latency to set
+	 */
+	public void setLatency(final String latency)
+	{
+		this.latency = latency;
+	}
+
+	/**
+	 * @return the errorList
+	 */
+	public List<ErrorList> getErrorList()
+	{
+		return errorList;
+	}
+
+	/**
+	 * @param errorList
+	 *           the errorList to set
+	 */
+	public void setErrorList(final List<ErrorList> errorList)
+	{
+		this.errorList = errorList;
+	}
+
+}
