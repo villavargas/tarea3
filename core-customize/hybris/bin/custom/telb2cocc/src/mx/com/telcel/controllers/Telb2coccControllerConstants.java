70ac0026aee497c924dc7caee1549cf852767d3f
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:16:12 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telb2cocc/src/mx/com/telcel/controllers/Telb2coccControllerConstants.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.controllers;
+
+/**
+ * Global class for all Telb2cocc constants. You can add global constants for your extension into this class.
+ */
+public interface Telb2coccControllerConstants
+{
+	public static final String LOCATION = "Location";
+	public static final String SLASH = "/";
+}
