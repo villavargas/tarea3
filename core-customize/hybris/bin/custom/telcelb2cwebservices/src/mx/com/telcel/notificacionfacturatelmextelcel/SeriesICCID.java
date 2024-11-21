10ee39ccc3f51e30c333084edb22a5ee82ee847e
villavargas <villa_vargas@hotmail.com>
2024-11-20 22:33:18 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/notificacionfacturatelmextelcel/SeriesICCID.java
@@ -0,0 +1,62 @@
+
+package mx.com.telcel.notificacionfacturatelmextelcel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for SeriesICCID complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="SeriesICCID"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="iccid" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SeriesICCID", propOrder = {
+    "iccid"
+})
+public class SeriesICCID {
+
+    @XmlElement(required = true)
+    protected String iccid;
+
+    /**
+     * Gets the value of the iccid property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIccid() {
+        return iccid;
+    }
+
+    /**
+     * Sets the value of the iccid property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIccid(String value) {
+        this.iccid = value;
+    }
+
+}
