09a715b8cea7fe84c0c0b1ec14f97f4e130218a4
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:23:09 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfulfilmentprocess/src/mx/com/telcel/fulfilmentprocess/constants/Telcelb2cFulfilmentProcessConstants.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.fulfilmentprocess.constants;
+
+@SuppressWarnings("deprecation")
+public final class Telcelb2cFulfilmentProcessConstants extends GeneratedTelcelb2cFulfilmentProcessConstants
+{
+	public static final String CONSIGNMENT_SUBPROCESS_END_EVENT_NAME = "ConsignmentSubprocessEnd";
+	public static final String TELMEX_CONSIGNMENT_SUBPROCESS_END_EVENT_NAME = "TelmexConsignmentSubprocessEnd";
+	public static final String REPLACEMENT_CONSIGNMENT_SUBPROCESS_END_EVENT_NAME = "ReplacementConsignmentSubprocessEnd";
+	public static final String ORDER_PROCESS_NAME = "order-process";
+	public static final String CONSIGNMENT_SUBPROCESS_NAME = "consignment-process";
+	public static final String ORDER_PROCESS_TELMEX_NAME = "order-process-telmex";
+	public static final String CONSIGNMENT_SUBPROCESS_TELMEX_NAME = "consignment-process-telmex";
+	public static final String ORDER_PROCESS_REPLACEMENT_NAME = "order-process-replacement";
+	public static final String CONSIGNMENT_SUBPROCESS_REPLACEMENT_NAME = "consignment-process-replacement";
+	public static final String WAIT_FOR_WAREHOUSE = "WaitForWarehouse";
+	public static final String WAIT_FOR_TELMEX_WAREHOUSE = "WaitForTelmexWarehouse";
+	public static final String REPLACEMENT_WAIT_FOR_WAREHOUSE = "ReplacementWaitForWarehouse";
+	public static final String CONSIGNMENT_PICKUP = "ConsignmentPickup";
+	public static final String CONSIGNMENT_COUNTER = "CONSIGNMENT_COUNTER";
+	public static final String PARENT_PROCESS = "PARENT_PROCESS";
+	public static final String CUSTOMER = "CUSTOMER";
+	public static final String BASE_STORE = "BASE_STORE";
+}
