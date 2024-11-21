f040d332664fb2623ebdeab4a09546e8505bbef2
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:23 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/GeographicAddress.java
@@ -0,0 +1,295 @@
+package mx.com.telcel.models.cvtdat;
+
+public class GeographicAddress
+{
+
+	private String city;
+	private String country;
+	private String formatted;
+	private String fullAddress;
+	private Boolean isPrimary;
+	private String locality;
+	private String postcode;
+	private String region;
+	private String stateOrProvince;
+	private String streeName;
+	private String streetNr;
+	private String streetNrLast;
+	private String streetNRLastSuffix;
+	private String streetNrSuffix;
+	private String streetSuffix;
+	private String streetType;
+
+	/**
+	 * @return the city
+	 */
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *           the city to set
+	 */
+	public void setCity(final String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry()
+	{
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *           the country to set
+	 */
+	public void setCountry(final String country)
+	{
+		this.country = country;
+	}
+
+	/**
+	 * @return the formatted
+	 */
+	public String getFormatted()
+	{
+		return formatted;
+	}
+
+	/**
+	 * @param formatted
+	 *           the formatted to set
+	 */
+	public void setFormatted(final String formatted)
+	{
+		this.formatted = formatted;
+	}
+
+	/**
+	 * @return the fullAddress
+	 */
+	public String getFullAddress()
+	{
+		return fullAddress;
+	}
+
+	/**
+	 * @param fullAddress
+	 *           the fullAddress to set
+	 */
+	public void setFullAddress(final String fullAddress)
+	{
+		this.fullAddress = fullAddress;
+	}
+
+	/**
+	 * @return the isPrimary
+	 */
+	public Boolean getIsPrimary()
+	{
+		return isPrimary;
+	}
+
+	/**
+	 * @param isPrimary
+	 *           the isPrimary to set
+	 */
+	public void setIsPrimary(final Boolean isPrimary)
+	{
+		this.isPrimary = isPrimary;
+	}
+
+	/**
+	 * @return the locality
+	 */
+	public String getLocality()
+	{
+		return locality;
+	}
+
+	/**
+	 * @param locality
+	 *           the locality to set
+	 */
+	public void setLocality(final String locality)
+	{
+		this.locality = locality;
+	}
+
+	/**
+	 * @return the postcode
+	 */
+	public String getPostcode()
+	{
+		return postcode;
+	}
+
+	/**
+	 * @param postcode
+	 *           the postcode to set
+	 */
+	public void setPostcode(final String postcode)
+	{
+		this.postcode = postcode;
+	}
+
+	/**
+	 * @return the region
+	 */
+	public String getRegion()
+	{
+		return region;
+	}
+
+	/**
+	 * @param region
+	 *           the region to set
+	 */
+	public void setRegion(final String region)
+	{
+		this.region = region;
+	}
+
+	/**
+	 * @return the stateOrProvince
+	 */
+	public String getStateOrProvince()
+	{
+		return stateOrProvince;
+	}
+
+	/**
+	 * @param stateOrProvince
+	 *           the stateOrProvince to set
+	 */
+	public void setStateOrProvince(final String stateOrProvince)
+	{
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	/**
+	 * @return the streeName
+	 */
+	public String getStreeName()
+	{
+		return streeName;
+	}
+
+	/**
+	 * @param streeName
+	 *           the streeName to set
+	 */
+	public void setStreeName(final String streeName)
+	{
+		this.streeName = streeName;
+	}
+
+	/**
+	 * @return the streetNr
+	 */
+	public String getStreetNr()
+	{
+		return streetNr;
+	}
+
+	/**
+	 * @param streetNr
+	 *           the streetNr to set
+	 */
+	public void setStreetNr(final String streetNr)
+	{
+		this.streetNr = streetNr;
+	}
+
+	/**
+	 * @return the streetNrLast
+	 */
+	public String getStreetNrLast()
+	{
+		return streetNrLast;
+	}
+
+	/**
+	 * @param streetNrLast
+	 *           the streetNrLast to set
+	 */
+	public void setStreetNrLast(final String streetNrLast)
+	{
+		this.streetNrLast = streetNrLast;
+	}
+
+	/**
+	 * @return the streetNRLastSuffix
+	 */
+	public String getStreetNRLastSuffix()
+	{
+		return streetNRLastSuffix;
+	}
+
+	/**
+	 * @param streetNRLastSuffix
+	 *           the streetNRLastSuffix to set
+	 */
+	public void setStreetNRLastSuffix(final String streetNRLastSuffix)
+	{
+		this.streetNRLastSuffix = streetNRLastSuffix;
+	}
+
+	/**
+	 * @return the streetNrSuffix
+	 */
+	public String getStreetNrSuffix()
+	{
+		return streetNrSuffix;
+	}
+
+	/**
+	 * @param streetNrSuffix
+	 *           the streetNrSuffix to set
+	 */
+	public void setStreetNrSuffix(final String streetNrSuffix)
+	{
+		this.streetNrSuffix = streetNrSuffix;
+	}
+
+	/**
+	 * @return the streetSuffix
+	 */
+	public String getStreetSuffix()
+	{
+		return streetSuffix;
+	}
+
+	/**
+	 * @param streetSuffix
+	 *           the streetSuffix to set
+	 */
+	public void setStreetSuffix(final String streetSuffix)
+	{
+		this.streetSuffix = streetSuffix;
+	}
+
+	/**
+	 * @return the streetType
+	 */
+	public String getStreetType()
+	{
+		return streetType;
+	}
+
+	/**
+	 * @param streetType
+	 *           the streetType to set
+	 */
+	public void setStreetType(final String streetType)
+	{
+		this.streetType = streetType;
+	}
+
+}
