99d3fcbed34fb8426e6432a6e1ea39c3c512e9c2
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:07:21 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/sap/inventario/ConsultarProductosPetType.java
@@ -0,0 +1,75 @@
+
+package com.amx.telcel.di.sds.esb.sap.inventario;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ConsultarProductosPetType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ConsultarProductosPetType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="MATNR" maxOccurs="unbounded"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;maxLength value="18"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ConsultarProductosPetType", propOrder = {
+    "matnr"
+})
+public class ConsultarProductosPetType {
+
+    @XmlElement(name = "MATNR", required = true)
+    protected List<String> matnr;
+
+    /**
+     * Gets the value of the matnr property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the matnr property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getMATNR().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<String> getMATNR() {
+        if (matnr == null) {
+            matnr = new ArrayList<String>();
+        }
+        return this.matnr;
+    }
+
+}
