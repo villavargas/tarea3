bc3ed3c33fb43482ce3d17771a37548170e64dd0
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:07 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/Quantity.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.individual;
+
+public class Quantity
+{
+
+	private Float amount;
+	private String units;
+
+	/**
+	 * @return the amount
+	 */
+	public Float getAmount()
+	{
+		return amount;
+	}
+
+	/**
+	 * @param amount
+	 *           the amount to set
+	 */
+	public void setAmount(final Float amount)
+	{
+		this.amount = amount;
+	}
+
+	/**
+	 * @return the units
+	 */
+	public String getUnits()
+	{
+		return units;
+	}
+
+	/**
+	 * @param units
+	 *           the units to set
+	 */
+	public void setUnits(final String units)
+	{
+		this.units = units;
+	}
+
+}
