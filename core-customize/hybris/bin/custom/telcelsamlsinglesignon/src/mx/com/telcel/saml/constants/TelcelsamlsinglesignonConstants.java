bc392bec2464aa47dd31c5e328bdf9248ff09871
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:12:38 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelsamlsinglesignon/src/mx/com/telcel/saml/constants/TelcelsamlsinglesignonConstants.java
@@ -0,0 +1,14 @@
+package mx.com.telcel.saml.constants;
+
+@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
+public class TelcelsamlsinglesignonConstants extends GeneratedTelcelsamlsinglesignonConstants
+{
+	public static final String EXTENSIONNAME = "telcelsamlsinglesignon";
+	
+	private TelcelsamlsinglesignonConstants()
+	{
+		//empty
+	}
+	
+	
+}
