acff408837f746e6e79a8c1fc43420ed1ab276ad
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:24:26 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/Quantity.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.fedex;
+
+public class Quantity
+{
+
+	private String unit;
+	private String amount;
+
+	/**
+	 * @return the unit
+	 */
+	public String getUnit()
+	{
+		return unit;
+	}
+
+	/**
+	 * @param unit
+	 *           the unit to set
+	 */
+	public void setUnit(final String unit)
+	{
+		this.unit = unit;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public String getAmount()
+	{
+		return amount;
+	}
+
+	/**
+	 * @param amount
+	 *           the amount to set
+	 */
+	public void setAmount(final String amount)
+	{
+		this.amount = amount;
+	}
+
+}
