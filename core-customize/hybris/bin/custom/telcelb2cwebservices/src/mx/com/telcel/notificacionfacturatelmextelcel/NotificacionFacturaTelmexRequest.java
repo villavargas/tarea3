c8d0ced550870b20d810afb959fe8e96dd7db029
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:18:10 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/notificacionfacturatelmextelcel/NotificacionFacturaTelmexRequest.java
@@ -0,0 +1,65 @@
+
+package mx.com.telcel.notificacionfacturatelmextelcel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>
+ * Java class for anonymous complex type.
+ *
+ * <p>
+ * The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="Factura" type="{http://ibcomercio/telcel.com/commerce/services/SAP/NotificacionFacturaTelmex/}Factura"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder =
+{ "factura" })
+@XmlRootElement(name = "NotificacionFacturaTelmexRequest")
+public class NotificacionFacturaTelmexRequest
+{
+
+	@XmlElement(name = "Factura", required = true)
+	protected Factura factura;
+
+	/**
+	 * Gets the value of the factura property.
+	 *
+	 * @return possible object is {@link Factura }
+	 *
+	 */
+	public Factura getFactura()
+	{
+		return factura;
+	}
+
+	/**
+	 * Sets the value of the factura property.
+	 *
+	 * @param value
+	 *           allowed object is {@link Factura }
+	 *
+	 */
+	public void setFactura(final Factura value)
+	{
+		this.factura = value;
+	}
+
+}
