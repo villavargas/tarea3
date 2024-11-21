e3a906d1fd66fda27aea4aea2ae6ae4be86180d4
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:24:30 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/MediumCharacteristic.java
@@ -0,0 +1,223 @@
+package mx.com.telcel.models.fedex;
+
+public class MediumCharacteristic
+{
+
+	private String city;
+	private String country;
+	private String emailAddress;
+	private String faxNumber;
+	private String phoneNumber;
+	private String phoneNumberExtension;
+	private String postCode;
+	private String stateOrProvince;
+	private String street1;
+	private String street2;
+	private String mobileNumber;
+	private String type;
+
+	/**
+	 * @return the city
+	 */
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *           the city to set
+	 */
+	public void setCity(final String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry()
+	{
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *           the country to set
+	 */
+	public void setCountry(final String country)
+	{
+		this.country = country;
+	}
+
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress()
+	{
+		return emailAddress;
+	}
+
+	/**
+	 * @param emailAddress
+	 *           the emailAddress to set
+	 */
+	public void setEmailAddress(final String emailAddress)
+	{
+		this.emailAddress = emailAddress;
+	}
+
+	/**
+	 * @return the faxNumber
+	 */
+	public String getFaxNumber()
+	{
+		return faxNumber;
+	}
+
+	/**
+	 * @param faxNumber
+	 *           the faxNumber to set
+	 */
+	public void setFaxNumber(final String faxNumber)
+	{
+		this.faxNumber = faxNumber;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *           the phoneNumber to set
+	 */
+	public void setPhoneNumber(final String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the phoneNumberExtension
+	 */
+	public String getPhoneNumberExtension()
+	{
+		return phoneNumberExtension;
+	}
+
+	/**
+	 * @param phoneNumberExtension
+	 *           the phoneNumberExtension to set
+	 */
+	public void setPhoneNumberExtension(final String phoneNumberExtension)
+	{
+		this.phoneNumberExtension = phoneNumberExtension;
+	}
+
+	/**
+	 * @return the postCode
+	 */
+	public String getPostCode()
+	{
+		return postCode;
+	}
+
+	/**
+	 * @param postCode
+	 *           the postCode to set
+	 */
+	public void setPostCode(final String postCode)
+	{
+		this.postCode = postCode;
+	}
+
+	/**
+	 * @return the stateOrProvince
+	 */
+	public String getStateOrProvince()
+	{
+		return stateOrProvince;
+	}
+
+	/**
+	 * @param stateOrProvince
+	 *           the stateOrProvince to set
+	 */
+	public void setStateOrProvince(final String stateOrProvince)
+	{
+		this.stateOrProvince = stateOrProvince;
+	}
+
+	/**
+	 * @return the street1
+	 */
+	public String getStreet1()
+	{
+		return street1;
+	}
+
+	/**
+	 * @param street1
+	 *           the street1 to set
+	 */
+	public void setStreet1(final String street1)
+	{
+		this.street1 = street1;
+	}
+
+	/**
+	 * @return the street2
+	 */
+	public String getStreet2()
+	{
+		return street2;
+	}
+
+	/**
+	 * @param street2
+	 *           the street2 to set
+	 */
+	public void setStreet2(final String street2)
+	{
+		this.street2 = street2;
+	}
+
+	/**
+	 * @return the mobileNumber
+	 */
+	public String getMobileNumber()
+	{
+		return mobileNumber;
+	}
+
+	/**
+	 * @param mobileNumber
+	 *           the mobileNumber to set
+	 */
+	public void setMobileNumber(final String mobileNumber)
+	{
+		this.mobileNumber = mobileNumber;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *           the type to set
+	 */
+	public void setType(final String type)
+	{
+		this.type = type;
+	}
+
+}
