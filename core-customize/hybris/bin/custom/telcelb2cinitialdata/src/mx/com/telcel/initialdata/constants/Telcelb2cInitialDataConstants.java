b3dcb6f2309f0a183d9fd2046bacdc11808cfc71
villavargas <villa_vargas@hotmail.com>
2024-11-21 13:57:44 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cinitialdata/src/mx/com/telcel/initialdata/constants/Telcelb2cInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.initialdata.constants;
+
+/**
+ * Global class for all Telcelb2cInitialData constants.
+ */
+public final class Telcelb2cInitialDataConstants extends GeneratedTelcelb2cInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "telcelb2cinitialdata";
+
+	private Telcelb2cInitialDataConstants()
+	{
+		//empty
+	}
+}
