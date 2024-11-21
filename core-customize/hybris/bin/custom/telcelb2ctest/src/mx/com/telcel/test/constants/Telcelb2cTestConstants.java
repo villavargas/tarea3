05fc8e117c0c6f3ff515707666ca133317a32aed
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:12:40 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2ctest/src/mx/com/telcel/test/constants/Telcelb2cTestConstants.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.test.constants;
+
+/**
+ * 
+ */
+public class Telcelb2cTestConstants extends GeneratedTelcelb2cTestConstants
+{
+
+	public static final String EXTENSIONNAME = "telcelb2ctest";
+
+}
