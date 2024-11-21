58135e982ccd45d2d65e35d6ecdfedf75040f660
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:04:35 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/sap/facturacion/BorraDocumentosRespType.java
@@ -0,0 +1,81 @@
+
+package com.amx.telcel.di.sds.esb.sap.facturacion;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for BorraDocumentosRespType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="BorraDocumentosRespType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="codigo" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="mensaje" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "BorraDocumentosRespType", propOrder = {
+    "codigo",
+    "mensaje"
+})
+public class BorraDocumentosRespType {
+
+    protected int codigo;
+    @XmlElement(required = true)
+    protected String mensaje;
+
+    /**
+     * Gets the value of the codigo property.
+     * 
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Sets the value of the codigo property.
+     * 
+     */
+    public void setCodigo(int value) {
+        this.codigo = value;
+    }
+
+    /**
+     * Gets the value of the mensaje property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    /**
+     * Sets the value of the mensaje property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMensaje(String value) {
+        this.mensaje = value;
+    }
+
+}
