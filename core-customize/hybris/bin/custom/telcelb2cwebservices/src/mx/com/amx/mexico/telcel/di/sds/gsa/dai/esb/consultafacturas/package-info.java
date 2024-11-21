b84fccb5c1ded9eb7fcfd0a2635f52a42131c0ca
villavargas <villa_vargas@hotmail.com>
2024-11-20 22:24:01 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/amx/mexico/telcel/di/sds/gsa/dai/esb/consultafacturas/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.amx.com.mx/mexico/telcel/di/sds/gsa/dai/esb/consultafacturas")
+package mx.com.amx.mexico.telcel.di.sds.gsa.dai.esb.consultafacturas;
