8baeb1d95ed22580a32ee03d5f2a94e2d52d283b
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:15:57 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfulfilmentprocess/src/mx/com/telcel/fulfilmentprocess/warehouse/DefaultWarehouse2ProcessAdapter.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.fulfilmentprocess.warehouse;
+
+import de.hybris.platform.commerceservices.enums.WarehouseConsignmentState;
+import de.hybris.platform.core.model.order.AbstractOrderModel;
+import de.hybris.platform.core.model.order.TelcelReplacementOrderModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentProcessModel;
+import de.hybris.platform.processengine.BusinessProcessService;
+import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.warehouse.Warehouse2ProcessAdapter;
+import de.hybris.platform.warehouse.WarehouseConsignmentStatus;
+import mx.com.telcel.fulfilmentprocess.constants.Telcelb2cFulfilmentProcessConstants;
+
+import java.util.Map;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Required;
+
+
+public class DefaultWarehouse2ProcessAdapter implements Warehouse2ProcessAdapter {
+    private Map<WarehouseConsignmentStatus, WarehouseConsignmentState> statusMap;
+    private ModelService modelService;
+
+    private BusinessProcessService businessProcessService;
+
+    @Override
+    public void receiveConsignmentStatus(final ConsignmentModel consignment, final WarehouseConsignmentStatus status) {
+        final AbstractOrderModel orderModel = consignment.getOrder();
+        for (final ConsignmentProcessModel process : consignment.getConsignmentProcesses()) {
+            final WarehouseConsignmentState state = getStatusMap().get(status);
+            if (state == null) {
+                throw new IllegalStateException("No mapping for WarehouseConsignmentStatus: " + status);
+            }
+            process.setWarehouseConsignmentState(state);
+            getModelService().save(process);
+            if (orderModel instanceof TelcelReplacementOrderModel) {
+                getBusinessProcessService().triggerEvent(
+                        process.getCode() + "_" + Telcelb2cFulfilmentProcessConstants.REPLACEMENT_WAIT_FOR_WAREHOUSE);
+            } else if (CollectionUtils.isEmpty(orderModel.getDeviceComboList())) {
+                getBusinessProcessService().triggerEvent(
+                        process.getCode() + "_" + Telcelb2cFulfilmentProcessConstants.WAIT_FOR_WAREHOUSE);
+            } else {
+                getBusinessProcessService().triggerEvent(
+                        process.getCode() + "_" + Telcelb2cFulfilmentProcessConstants.WAIT_FOR_TELMEX_WAREHOUSE);
+            }
+        }
+    }
+
+    protected Map<WarehouseConsignmentStatus, WarehouseConsignmentState> getStatusMap() {
+        return statusMap;
+    }
+
+    @Required
+    public void setStatusMap(final Map<WarehouseConsignmentStatus, WarehouseConsignmentState> statusMap) {
+        this.statusMap = statusMap;
+    }
+
+    protected ModelService getModelService() {
+        return modelService;
+    }
+
+    @Required
+    public void setModelService(final ModelService modelService) {
+        this.modelService = modelService;
+    }
+
+    protected BusinessProcessService getBusinessProcessService() {
+        return businessProcessService;
+    }
+
+    @Required
+    public void setBusinessProcessService(final BusinessProcessService businessProcessService) {
+        this.businessProcessService = businessProcessService;
+    }
+}
