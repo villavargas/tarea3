bc59c6786382f924631ca3bb0f3419f88845c49f
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:07:48 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/logistica/fedex/rate/Party.java
@@ -0,0 +1,118 @@
+
+package com.amx.telcel.di.sds.esb.logistica.fedex.rate;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for Party complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Party"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="AccountNumber" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="Contact" type="{http://www.amx.com/telcel/di/sds/esb/logistica/fedex/rate}Contact" minOccurs="0"/&gt;
+ *         &lt;element name="Address" type="{http://www.amx.com/telcel/di/sds/esb/logistica/fedex/rate}Address" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Party", propOrder = {
+    "accountNumber",
+    "contact",
+    "address"
+})
+public class Party {
+
+    @XmlElement(name = "AccountNumber")
+    protected String accountNumber;
+    @XmlElement(name = "Contact")
+    protected Contact contact;
+    @XmlElement(name = "Address")
+    protected Address address;
+
+    /**
+     * Gets the value of the accountNumber property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * Sets the value of the accountNumber property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAccountNumber(String value) {
+        this.accountNumber = value;
+    }
+
+    /**
+     * Gets the value of the contact property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Contact }
+     *     
+     */
+    public Contact getContact() {
+        return contact;
+    }
+
+    /**
+     * Sets the value of the contact property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Contact }
+     *     
+     */
+    public void setContact(Contact value) {
+        this.contact = value;
+    }
+
+    /**
+     * Gets the value of the address property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Address }
+     *     
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the value of the address property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Address }
+     *     
+     */
+    public void setAddress(Address value) {
+        this.address = value;
+    }
+
+}
