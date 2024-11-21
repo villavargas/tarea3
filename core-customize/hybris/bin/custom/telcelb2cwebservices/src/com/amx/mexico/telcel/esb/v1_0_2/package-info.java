1566a0de9d80c0f7110e6c13777094bd16aefa9f
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:25:03 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/mexico/telcel/esb/v1_0_2/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://amx.com/mexico/telcel/esb/v1_0_2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.amx.mexico.telcel.esb.v1_0_2;
