ac9c9d3801777911d5305cb1116c59d50a5b11cb
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:23:29 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telb2cocc/src/mx/com/telcel/constants/Telb2coccWebConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.constants;
+
+/**
+ * Global class for all Telb2cocc web constants. You can add global constants for your extension into this class.
+ */
+public final class Telb2coccWebConstants
+{
+	private Telb2coccWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
