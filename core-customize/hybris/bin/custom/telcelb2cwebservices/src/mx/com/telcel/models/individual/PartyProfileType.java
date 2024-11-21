482f4c74d494844ec9785340d6d0cdbdee1dacaa
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:17:12 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/individual/PartyProfileType.java
@@ -0,0 +1,79 @@
+package mx.com.telcel.models.individual;
+
+public class PartyProfileType
+{
+
+	private String id;
+	private String description;
+	private String name;
+	private String category;
+
+	/**
+	 * @return the id
+	 */
+	public String getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(final String id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *           the description to set
+	 */
+	public void setDescription(final String description)
+	{
+		this.description = description;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public String getCategory()
+	{
+		return category;
+	}
+
+	/**
+	 * @param category
+	 *           the category to set
+	 */
+	public void setCategory(final String category)
+	{
+		this.category = category;
+	}
+
+}
