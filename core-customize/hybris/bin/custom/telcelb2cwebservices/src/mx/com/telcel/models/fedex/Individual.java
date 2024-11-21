6c7fb7b3ded61fe6c834c21d6085166a12fdf0fc
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:24:25 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/fedex/Individual.java
@@ -0,0 +1,133 @@
+package mx.com.telcel.models.fedex;
+
+public class Individual
+{
+
+	private String name;
+	private String lastName;
+	private String secondLastName;
+	private String birthDate;
+	private String birthPlace;
+	private String categoryName;
+	private String gender;
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *           the lastName to set
+	 */
+	public void setLastName(final String lastName)
+	{
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the secondLastName
+	 */
+	public String getSecondLastName()
+	{
+		return secondLastName;
+	}
+
+	/**
+	 * @param secondLastName
+	 *           the secondLastName to set
+	 */
+	public void setSecondLastName(final String secondLastName)
+	{
+		this.secondLastName = secondLastName;
+	}
+
+	/**
+	 * @return the birthDate
+	 */
+	public String getBirthDate()
+	{
+		return birthDate;
+	}
+
+	/**
+	 * @param birthDate
+	 *           the birthDate to set
+	 */
+	public void setBirthDate(final String birthDate)
+	{
+		this.birthDate = birthDate;
+	}
+
+	/**
+	 * @return the birthPlace
+	 */
+	public String getBirthPlace()
+	{
+		return birthPlace;
+	}
+
+	/**
+	 * @param birthPlace
+	 *           the birthPlace to set
+	 */
+	public void setBirthPlace(final String birthPlace)
+	{
+		this.birthPlace = birthPlace;
+	}
+
+	/**
+	 * @return the categoryName
+	 */
+	public String getCategoryName()
+	{
+		return categoryName;
+	}
+
+	/**
+	 * @param categoryName
+	 *           the categoryName to set
+	 */
+	public void setCategoryName(final String categoryName)
+	{
+		this.categoryName = categoryName;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender()
+	{
+		return gender;
+	}
+
+	/**
+	 * @param gender
+	 *           the gender to set
+	 */
+	public void setGender(final String gender)
+	{
+		this.gender = gender;
+	}
+
+}
