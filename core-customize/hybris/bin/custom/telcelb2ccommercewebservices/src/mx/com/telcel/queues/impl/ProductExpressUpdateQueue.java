ba1dadda984be48de8b8793ac8f7dd395737536e
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:00:01 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2ccommercewebservices/src/mx/com/telcel/queues/impl/ProductExpressUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.queues.impl;
+
+import mx.com.telcel.queues.data.ProductExpressUpdateElementData;
+
+
+/**
+ * Queue for {@link mx.com.telcel.queues.data.ProductExpressUpdateElementData}
+ */
+public class ProductExpressUpdateQueue extends AbstractUpdateQueue<ProductExpressUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
