b41072338b2f931847e9a1937dd1d922142f8bfb
villavargas <villa_vargas@hotmail.com>
2024-11-20 20:13:16 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/notificacionfacturatelmextelcel/SeriesIMEI.java
@@ -0,0 +1,62 @@
+
+package mx.com.telcel.notificacionfacturatelmextelcel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for SeriesIMEI complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="SeriesIMEI"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="imei" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SeriesIMEI", propOrder = {
+    "imei"
+})
+public class SeriesIMEI {
+
+    @XmlElement(required = true)
+    protected String imei;
+
+    /**
+     * Gets the value of the imei property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getImei() {
+        return imei;
+    }
+
+    /**
+     * Sets the value of the imei property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setImei(String value) {
+        this.imei = value;
+    }
+
+}
