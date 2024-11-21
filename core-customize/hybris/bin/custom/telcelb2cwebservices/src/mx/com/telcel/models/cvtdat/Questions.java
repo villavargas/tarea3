8a441ab2d45a275da8b53f4f30f789511870e6a1
villavargas <villa_vargas@hotmail.com>
2024-11-20 21:09:22 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/Questions.java
@@ -0,0 +1,169 @@
+package mx.com.telcel.models.cvtdat;
+
+public class Questions
+{
+
+	private String id;
+	private String name;
+	private String description;
+	private String type;
+	private String category;
+	private String answer;
+	private String longitude;
+	private String mask;
+	private String dataType;
+
+	/**
+	 * @return the id
+	 */
+	public String getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *           the id to set
+	 */
+	public void setId(final String id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *           the description to set
+	 */
+	public void setDescription(final String description)
+	{
+		this.description = description;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *           the type to set
+	 */
+	public void setType(final String type)
+	{
+		this.type = type;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public String getCategory()
+	{
+		return category;
+	}
+
+	/**
+	 * @param category
+	 *           the category to set
+	 */
+	public void setCategory(final String category)
+	{
+		this.category = category;
+	}
+
+	/**
+	 * @return the answer
+	 */
+	public String getAnswer()
+	{
+		return answer;
+	}
+
+	/**
+	 * @param answer
+	 *           the answer to set
+	 */
+	public void setAnswer(final String answer)
+	{
+		this.answer = answer;
+	}
+
+	/**
+	 * @return the longitude
+	 */
+	public String getLongitude()
+	{
+		return longitude;
+	}
+
+	/**
+	 * @param longitude
+	 *           the longitude to set
+	 */
+	public void setLongitude(final String longitude)
+	{
+		this.longitude = longitude;
+	}
+
+	/**
+	 * @return the mask
+	 */
+	public String getMask()
+	{
+		return mask;
+	}
+
+	/**
+	 * @param mask
+	 *           the mask to set
+	 */
+	public void setMask(final String mask)
+	{
+		this.mask = mask;
+	}
+
+	/**
+	 * @return the dataType
+	 */
+	public String getDataType()
+	{
+		return dataType;
+	}
+
+	/**
+	 * @param dataType
+	 *           the dataType to set
+	 */
+	public void setDataType(final String dataType)
+	{
+		this.dataType = dataType;
+	}
+
+}
