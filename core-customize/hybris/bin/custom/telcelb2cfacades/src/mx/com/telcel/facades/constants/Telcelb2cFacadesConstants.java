23f58d37aa33b68122330145a4d049b453462d32
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:47:44 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cfacades/src/mx/com/telcel/facades/constants/Telcelb2cFacadesConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package mx.com.telcel.facades.constants;
+
+/**
+ * Global class for all Telcelb2cFacades constants.
+ */
+public class Telcelb2cFacadesConstants extends GeneratedTelcelb2cFacadesConstants
+{
+	public static final String EXTENSIONNAME = "telcelb2cfacades";
+
+	private Telcelb2cFacadesConstants()
+	{
+		//empty
+	}
+}
