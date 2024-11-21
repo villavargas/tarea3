9cce2e69200c7d871ec225a8e4bd7654acac0e98
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:09:03 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/mx/com/telcel/models/cvtdat/Characteristic.java
@@ -0,0 +1,43 @@
+package mx.com.telcel.models.cvtdat;
+
+public class Characteristic
+{
+
+	private String name;
+	private String value;
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *           the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *           the value to set
+	 */
+	public void setValue(final String value)
+	{
+		this.value = value;
+	}
+
+}
