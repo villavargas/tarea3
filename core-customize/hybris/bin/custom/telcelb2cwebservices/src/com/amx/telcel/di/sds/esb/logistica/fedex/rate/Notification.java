e24a0022d448b15b1295b0477e127d04d16ea634
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:59:56 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/logistica/fedex/rate/Notification.java
@@ -0,0 +1,179 @@
+
+package com.amx.telcel.di.sds.esb.logistica.fedex.rate;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * The descriptive data regarding the result of the
+ * 				submitted transaction.
+ * 
+ * <p>Java class for Notification complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Notification"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="Severity" type="{http://www.amx.com/telcel/di/sds/esb/logistica/fedex/rate}NotificationSeverityType" minOccurs="0"/&gt;
+ *         &lt;element name="Source" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="Code" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="Message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="LocalizedMessage" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Notification", propOrder = {
+    "severity",
+    "source",
+    "code",
+    "message",
+    "localizedMessage"
+})
+public class Notification {
+
+    @XmlElement(name = "Severity")
+    @XmlSchemaType(name = "string")
+    protected NotificationSeverityType severity;
+    @XmlElement(name = "Source")
+    protected String source;
+    @XmlElement(name = "Code")
+    protected String code;
+    @XmlElement(name = "Message")
+    protected String message;
+    @XmlElement(name = "LocalizedMessage")
+    protected String localizedMessage;
+
+    /**
+     * Gets the value of the severity property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link NotificationSeverityType }
+     *     
+     */
+    public NotificationSeverityType getSeverity() {
+        return severity;
+    }
+
+    /**
+     * Sets the value of the severity property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link NotificationSeverityType }
+     *     
+     */
+    public void setSeverity(NotificationSeverityType value) {
+        this.severity = value;
+    }
+
+    /**
+     * Gets the value of the source property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Sets the value of the source property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSource(String value) {
+        this.source = value;
+    }
+
+    /**
+     * Gets the value of the code property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Sets the value of the code property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCode(String value) {
+        this.code = value;
+    }
+
+    /**
+     * Gets the value of the message property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets the value of the message property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+    /**
+     * Gets the value of the localizedMessage property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLocalizedMessage() {
+        return localizedMessage;
+    }
+
+    /**
+     * Sets the value of the localizedMessage property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLocalizedMessage(String value) {
+        this.localizedMessage = value;
+    }
+
+}
