a533e12518938898bbdadae840947ee72eee05d1
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:58:21 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/logistica/fedex/rate/Tax.java
@@ -0,0 +1,120 @@
+
+package com.amx.telcel.di.sds.esb.logistica.fedex.rate;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for Tax complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Tax"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="TaxType" type="{http://www.amx.com/telcel/di/sds/esb/logistica/fedex/rate}TaxType" minOccurs="0"/&gt;
+ *         &lt;element name="Description" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="Amount" type="{http://www.amx.com/telcel/di/sds/esb/logistica/fedex/rate}Money" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Tax", propOrder = {
+    "taxType",
+    "description",
+    "amount"
+})
+public class Tax {
+
+    @XmlElement(name = "TaxType")
+    @XmlSchemaType(name = "string")
+    protected TaxType taxType;
+    @XmlElement(name = "Description")
+    protected String description;
+    @XmlElement(name = "Amount")
+    protected Money amount;
+
+    /**
+     * Gets the value of the taxType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link TaxType }
+     *     
+     */
+    public TaxType getTaxType() {
+        return taxType;
+    }
+
+    /**
+     * Sets the value of the taxType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link TaxType }
+     *     
+     */
+    public void setTaxType(TaxType value) {
+        this.taxType = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    /**
+     * Gets the value of the amount property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Money }
+     *     
+     */
+    public Money getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sets the value of the amount property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Money }
+     *     
+     */
+    public void setAmount(Money value) {
+        this.amount = value;
+    }
+
+}
