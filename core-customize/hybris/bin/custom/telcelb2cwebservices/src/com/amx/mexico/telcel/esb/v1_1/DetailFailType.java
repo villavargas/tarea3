84eac0acfaff9dc0639da52a0419968c169c9af5
villavargas <villa_vargas@hotmail.com>
2024-11-21 00:06:51 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/mexico/telcel/esb/v1_1/DetailFailType.java
@@ -0,0 +1,69 @@
+
+package com.amx.mexico.telcel.esb.v1_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for DetailFailType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="DetailFailType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="errors" type="{http://amx.com/mexico/telcel/esb/v1_1}ErrorType" maxOccurs="unbounded"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "DetailFailType", propOrder = {
+    "errors"
+})
+public class DetailFailType {
+
+    @XmlElement(required = true)
+    protected List<ErrorType> errors;
+
+    /**
+     * Gets the value of the errors property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the errors property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getErrors().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link ErrorType }
+     * 
+     * 
+     */
+    public List<ErrorType> getErrors() {
+        if (errors == null) {
+            errors = new ArrayList<ErrorType>();
+        }
+        return this.errors;
+    }
+
+}
