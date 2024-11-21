4f3fd77423b48251101fff44367136b582131b23
villavargas <villa_vargas@hotmail.com>
2024-11-20 23:58:49 -0600

    Modified by www.SourceFiles.app

---
+++ core-customize/hybris/bin/custom/telcelb2cwebservices/src/com/amx/telcel/di/sds/esb/logistica/fedex/rate/TransactionDetail.java
@@ -0,0 +1,62 @@
+
+package com.amx.telcel.di.sds.esb.logistica.fedex.rate;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for TransactionDetail complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="TransactionDetail"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="CustomerTransactionId" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "TransactionDetail", propOrder = {
+    "customerTransactionId"
+})
+public class TransactionDetail {
+
+    @XmlElement(name = "CustomerTransactionId")
+    protected String customerTransactionId;
+
+    /**
+     * Gets the value of the customerTransactionId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCustomerTransactionId() {
+        return customerTransactionId;
+    }
+
+    /**
+     * Sets the value of the customerTransactionId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCustomerTransactionId(String value) {
+        this.customerTransactionId = value;
+    }
+
+}
